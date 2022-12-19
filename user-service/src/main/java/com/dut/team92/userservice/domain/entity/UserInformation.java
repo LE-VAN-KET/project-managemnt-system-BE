74cdd9cacee2c6a699cb65d71680d1e07a69512b
LE-VAN-KET <anhle1512001@gmail.com>
2022-12-19 16:16:38 +0700

    fix: signup error share primary key user id

--- user-service/src/main/java/com/dut/team92/userservice/domain/entity/UserInformation.java
+++ user-service/src/main/java/com/dut/team92/userservice/domain/entity/UserInformation.java
@@ -4,6 +4,7 @@ import com.dut.team92.common.domain.BaseDomain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -18,7 +19,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class UserInformation extends BaseDomain {
     @Id
-    @Column(name = "user_information_id", unique = true, nullable = false, columnDefinition = "BINARY(16)")
+    @Column(name = "user_id", columnDefinition = "BINARY(16)")
     private UUID id;
 
     @Column(length = 50)
@@ -33,8 +34,8 @@ public class UserInformation extends BaseDomain {
     private Boolean mustChangePassword;
     private Instant passChangeAt;
 
+    @OneToOne(mappedBy = "userInformation")
+    @JoinColumn(name = "user_id")
     @MapsId
-    @OneToOne(mappedBy = "userInformation", fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_information_id")
     private User user;
 }
